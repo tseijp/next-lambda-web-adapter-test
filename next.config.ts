e4f2f7677d90291554280188959499001c7ee6d2
tseijp <_@tsei.jp>
2024-12-17 14:06:59 +0900

    Initial commit from Create Next App

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: "standalone",
+  distDir: "dist",
+};
+
+export default nextConfig;
